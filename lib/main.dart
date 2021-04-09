378a580d8838c04ba6417b0ca299dcbd881a28f4
Anwarul Islam <anwaarulislaam@gmail.com>
2021-04-10 01:40:44 +0600

    initial release

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tenms/constants.dart';
+import 'package:wakelock/wakelock.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +9,15 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Wakelock.enable();
+
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Colors.teal.shade100,
+        backgroundColor: Colors.blueGrey,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50.0,
@@ -22,10 +26,39 @@ class MyApp extends StatelessWidget {
               Text(
                 'Anwarul Islam',
                 style: TextStyle(
-                  fontSize: 20,
+                  color: Colors.white,
+                  fontSize: 40,
+                  fontFamily: 'Barlow',
                   fontWeight: FontWeight.bold,
                 ),
-              )
+              ),
+              Text(
+                'AWESOME DEVELOPER',
+                style: TextStyle(
+                  color: Colors.white70,
+                  fontFamily: 'Barlow',
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                width: 200,
+                child: Divider(
+                  color: Colors.white38,
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.phone_outlined),
+                  title: Text('+880 1819 400 400'),
+                ),
+              ),
+              Card(
+                child: ListTile(
+                  leading: Icon(Icons.email_outlined),
+                  title: Text('anwaarulislaam@gmail.com'),
+                ),
+              ),
             ],
           ),
         ),
